80304bcb370d0d9182e3812b6901587a787e8a29
Peter <pet.hubina@gmail.com>
2021-07-14 12:12:35 +0200

    Updates

---
+++ precache-manifest.686253ac45525cf7b078bc108a9b8481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7bd0cc32c03e0aba6e19d353f5ad41",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "90a5602a99d98d3ce6a7",
+    "url": "/color-generator/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "e2f04fb3c7f8121c70c2",
+    "url": "/color-generator/static/js/2.adb8e479.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-generator/static/js/2.adb8e479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a5602a99d98d3ce6a7",
+    "url": "/color-generator/static/js/main.1c1780fd.chunk.js"
+  },
+  {
+    "revision": "a1cfb88a1dc1d9e37dd7",
+    "url": "/color-generator/static/js/runtime-main.8a33e40c.js"
+  }
+]);
